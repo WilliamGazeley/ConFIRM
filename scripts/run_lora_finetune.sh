3a540508cefb1544da7a04e0253763a9f8acf621
adrwong <wongsiuho561@gmail.com>
2023-09-25 18:35:01 +0800

    bug fix for tuning script

--- scripts/run_lora_finetune.sh
+++ scripts/run_lora_finetune.sh
@@ -1,11 +1,11 @@
 python llama_2_lora_tune.py \
- --model_path /home/adrianw/everything/nlp/model-serving-sandbox/llama/llama-2-7b/hf \
- --save_path /home/adrianw/everything/nlp/model-serving-sandbox/llama/llama-2-7b/tuned \
+ --model_path /home/adrianw/hf \
+ --save_path /home/adrianw/tuned \
  --r 8 \
  --lora_dropout 0.05 \
  --lora_alpha 32 \
- --batch_size 4 \
- --epochs 10 \
+ --batch_size 8 \
+ --epochs 50 \
  --lr 0.0001 \
  --max_length 128 \
- --dataset_path /home/adrianw/everything/final/ConFIRM/datasets/chagpt/ConFIRM_QAset_gpt_filtered.csv
+ --dataset_path /home/adrianw/ConFIRM/datasets/mixed/ConFIRM_QAset_559n_mixed.csv
