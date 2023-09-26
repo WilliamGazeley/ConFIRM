ac8fc573905fb68242a440eddac45820199bfc6b
adrwong <wongsiuho561@gmail.com>
2023-09-26 17:38:36 +0800

    added ptune script

---
+++ scripts/run_ptune_finetune.sh
@@ -0,0 +1,10 @@
+python llama_2_ptune_tune.py \
+ --model_path /home/adrianw/hf \
+ --save_path /home/adrianw/tuned \
+ --num_virtual_tokens 30 \
+ --encoder_hidden_size 128 \
+ --batch_size 8 \
+ --epochs 50 \
+ --lr 0.01 \
+ --max_length 128 \
+ --dataset_path /home/adrianw/ConFIRM/datasets/mixed/ConFIRM_QAset_559n_mixed_train.csv
